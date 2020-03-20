923d8fcb08dbe5dbe677416a3d768e50ce2ab83f
JesusLR <jesus_pumas_12@hotmail.com>
2020-03-19 23:15:06 -0600

    L

--- resources/views/inicio/inicio.blade.php
+++ resources/views/inicio/inicio.blade.php
@@ -3,7 +3,7 @@
 
 <div class="card shadow mb-4">
     <div class="card-header py-3">
-      <h1 class="m-0 font-weight-bold text-primary">BIENVENIDO</h1>
+      <h1 class="m-0 font-weight-bold text-primary">BIENVENIDOssss</h1>
       <div class="my-2"></div>
     </div>
     <div class="card-body">
